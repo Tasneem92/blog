7ff55fedb3b353a8399e76bad5a537fd91df0421
Tasneem92 <tasneem.rawashdeh92@gmail.com>
2017-12-22 14:35:57 +0200

    First upload

---
+++ blog/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+# get_queryset is the method that returns the QuerySet to be executed. We use it to include our custom filter in the final QuerySet
+# We have defined our custom manager and added it to the Post model; we can use it to perform queries
+# For example, we can retrieve all the published posts whose titles starts with 'My' using:
+# Post.published.filter(title__startswith='My')
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES= (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    )
+    print(type(STATUS_CHOICES))
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User,related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+    objects = models.Manager() # The default manager
+    published = PublishedManager() # Our custom manager
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
+
+    # We are using strftime() to build the URL using month and day with leading zeros, we will use the get_absolute_url() in our templates
---
+++ blog/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from . models import Post
+from . forms import EmailPostForm
+from django.core.mail import send_mail
+
+# Create your views here.
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+    # posts = Post.published.all()
+
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_list(request, category=None):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+
+# The post_list view takes the request object as the only parameter,
+# retrieving all the posts with the published status using the published manager we created previously
+# The render() to render the list of posts with the given template
+# This function takes the request object as parameter, the template path & variables to render the given template.
+# It return an HttpResponse object with the rendered text (normally HTML code)
+# The render() takes the request context into account, so any variable set by template context processors is accessible by the given template
+
+
+def post_share(request, post_id):
+    # Retrieves post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            # retrieving the absolute path of the post to include a link to the post in the email
+            # We use this path as input for request.build_absolute_uri() to build a complete URL including HTTP schema and hostname
+
+            # We build the subject & the message body of the email using the cleaned data of the validated form
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+            # .. send email
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent})
